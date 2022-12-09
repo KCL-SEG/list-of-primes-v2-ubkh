d6523bcc0b1f39aaea62f1bd81adb651a619e35c
Ubayd Khan <ubaydkh@gmail.com>
2022-12-09 12:40:12 +0000

    Complete assignment

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be a positive integer")
+    number = 2
+    while len(list) < number_of_primes:
+        is_prime = True
+        for prime in list:
+            if number % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(number)
+        number += 1
     return list
